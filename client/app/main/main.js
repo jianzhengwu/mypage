4b6fd3bce74b830bbab68e41caaa48a85f3f982a
Jianzheng Wu <jianzheng.wu@gmail.com>
2015-07-16 22:00:23 +0200

    tonight

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -3,7 +3,7 @@
 angular.module('mypageApp')
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/main', {
+      .when('/', {
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       });
